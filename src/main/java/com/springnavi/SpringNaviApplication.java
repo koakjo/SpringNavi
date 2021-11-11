b53156d1690526d28272abad0241cb759bef3090
koakjo <kajitakemotohiko2@gmail.com>
2021-11-12 00:48:13 +0900

    springSterterSet commit

---
+++ src/main/java/com/springnavi/SpringNaviApplication.java
@@ -0,0 +1,13 @@
+package com.springnavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springnavi/SpringNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springnavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
